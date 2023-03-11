ffed521391622c540b742f0a920e15483a996c92
rokibhassanleon <rokibhassanleon@gmail.com>
2023-03-11 15:53:39 +0600

    Introduced AppointmentService and API Endpoint
    Added new API to get Monthly Revenue and Cost of selected practitioner and Date Range
    
    Added new API to get Monthly appointments of selected practitioner by specific month and year
    
    Added new API to get appointment details of a specific appointment

--- coreplus-sample-api/Program.cs
+++ coreplus-sample-api/Program.cs
@@ -1,12 +1,20 @@
+using Coreplus.Sample.Api.Endpoints.Appointment;
 using Coreplus.Sample.Api.Endpoints.Practitioner;
 using Coreplus.Sample.Api.Services;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<PractitionerService>();
-
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+	options.SerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
+});
 var app = builder.Build();
 
 var practitionerEndpoints = app.MapGroup("/practitioners");
 practitionerEndpoints.MapPractitionerEndpoints();
+
+var appointmentEndpoints = app.MapGroup("/appointments");
+appointmentEndpoints.MapAppointmentEndpoints();
 
 app.Run();
---
+++ coreplus-sample-api/Types/Appointments.cs
@@ -0,0 +1,3 @@
+﻿namespace Coreplus.Sample.Api.Types;
+
+public record Appointments(long id, DateTimeOffset date, string client_name, string appointment_type, int duration, decimal revenue, decimal cost, long practitioner_id);
