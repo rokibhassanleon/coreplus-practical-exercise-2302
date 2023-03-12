d2e8bc6606aa67cf52f6ee5e28e8bdcc3b3466eb
rokibhassan <rokib572@gmail.com>
2023-03-12 11:22:24 +0600

    Updated API
    Changed Response Data
    Added GetRemainingPractitioner API call
    Updated Appointment Service

--- coreplus-sample-api/Services/PractitionerService.cs
+++ coreplus-sample-api/Services/PractitionerService.cs
@@ -45,4 +45,23 @@ public class PractitionerService
 
         return new APIResponse<IEnumerable<PractitionerDto>> { statusCode = 200, data = result };
 	}
+
+	public async Task<APIResponse<IEnumerable<PractitionerDto>>> GetRemainingPractitioners()
+	{
+		using var fileStream = File.OpenRead(@"./Data/practitioners.json");
+		var data = await JsonSerializer.DeserializeAsync<Practitioner[]>(fileStream);
+		if (data == null)
+		{
+			throw new Exception("Data read error");
+		}
+
+		var result = data.Where(practitioner => (int)practitioner.level < 2).Select(prac => new PractitionerDto(prac.id, prac.name));
+
+		if (!result.Any())
+		{
+			return new APIResponse<IEnumerable<PractitionerDto>>() { statusCode = 404, message = "No record found!" };
+		}
+
+		return new APIResponse<IEnumerable<PractitionerDto>> { statusCode = 200, data = result };
+	}
 }
